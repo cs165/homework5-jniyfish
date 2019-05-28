e71cf898a8e08e53d890a1bbbbd897af3a4af009
jniyfish <39803423+jniyfish@users.noreply.github.com>
2019-05-28 17:29:15 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -20,12 +20,11 @@ async function onGet(req, res) {
   console.log(rows);
 
   var array = [];
-  
+
   for (var i = 1; i < rows.length; i++) {
     let json = {};
-    for(var j=0;j<rows[0].length;j++)
-    {
-      json[rows[0][j]]=rows[i][j];
+    for (var j = 0; j < rows[0].length; j++) {
+      json[rows[0][j]] = rows[i][j];
     }
     array.push(json);;
     console.log(array);
@@ -34,29 +33,30 @@ async function onGet(req, res) {
   res.json(array);
 
 
-  
+
 
 }
 app.get('/api', onGet);
 
 async function onPost(req, res) {
   const messageBody = req.body;
-  var newrow=[];
-  newrow[0]= messageBody[0];
-  newrow[1]= messageBody[1];
   const idList = Object.keys(messageBody);
-  const nameList = Object.values(messageBody);
-  //sheet.appendRow("message","312312");
-  //sheet.appendRow(["hello","31231@3213"]);
-  //sheet.appendRow(newrow);
-
-
+  var nameList = Object.values(messageBody);
+  const result = await sheet.getRows();
   console.log(idList);
   console.log(nameList);
-  sheet.appendRow(nameList);
-  // TODO(you): Implement onPost.
+  if (idList[0] == "name" && idList[1] == "email") {
+    sheet.appendRow(nameList);
+    res.json("success");
+  }
+  else if (idList[1] == "name" && idList[0] == "email") {
+    let tmp = nameList[1];
+    nameList[1] = nameList[0];
+    nameList[0] = tmp;
+    sheet.appendRow(nameList);
+    res.json("success");
+  }
 
-  res.json({ status: 'unimplemented' });
 }
 app.post('/api', jsonParser, onPost);
 
@@ -64,7 +64,41 @@ async function onPatch(req, res) {
   const column = req.params.column;
   const value = req.params.value;
   const messageBody = req.body;
+  const idList = Object.keys(messageBody);
+  var nameList = Object.values(messageBody);
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  var response={};
+  var newrow=[];
 
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < rows[i].length; j++) {
+      if (rows[i][j] == value) {
+        if(column=="name")
+        {
+          newrow[0]=rows[i][0];
+          newrow[1]=nameList[0];
+          sheet.setRow(i,newrow);
+          response[idList[0]]=nameList[0];
+          res.json(response);
+          break;
+        }
+        else if(column=="email")
+        {
+          newrow[1]=rows[i][1];
+          newrow[0]=nameList[0];
+          sheet.setRow(i,newrow);
+          response[idList[0]]=nameList[0];
+          res.json(response);
+          break;
+        }
+        else{
+          res.json({ "response": "success" });
+        }
+
+      }
+    }
+  }
   // TODO(you): Implement onPatch.
 
   res.json({ status: 'unimplemented' });
@@ -76,21 +110,15 @@ async function onDelete(req, res) {
   const value = req.params.value;
   const result = await sheet.getRows();
   const rows = result.rows;
-  for(let i=0 ;i<rows.length;i++)
-  {
-    for(let j=0 ;j<rows[i].length;j++)
-    {
-      if(rows[i][j]==value)
-      {
-          sheet.deleteRow(i);
-          res.json({"response":"success"});
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < rows[i].length; j++) {
+      if (rows[i][j] == value) {
+        sheet.deleteRow(i);
+        res.json({ "response": "success" });
+        break;
       }
     }
   }
-
-  // TODO(you): Implement onDelete.
-
- // res.json(rows[1]);
 }
 app.delete('/api/:column/:value', onDelete);
 
