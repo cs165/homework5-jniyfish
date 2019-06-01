f357111b3b9b6037ae49c7fffffe9ae5e81e2223
jniyfish <39803423+jniyfish@users.noreply.github.com>
2019-06-01 23:35:25 +0800

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -3,103 +3,131 @@
 //
 
 (() => {
-  async function onSubmit(event) {
-    event.preventDefault();
-    const resultsDiv = document.querySelector('#results');
-    resultsDiv.innerHTML = '';
+    async function onSubmit(event) {
+        event.preventDefault();
+        const resultsDiv = document.querySelector('#results');
+        const nameListDiv = document.querySelector('#NameList');
+        const uName = document.querySelector('#userName');
+        resultsDiv.innerHTML = '';
 
-    const info = getParameters();
+        const info = getParameters();
 
-    const response = await fetch(info.path, info.options);
-    const json = await response.json();
-    resultsDiv.textContent = JSON.stringify(json, null, 2);
+        const response = await fetch(info.path, info.options);
+        const json = await response.json();
+        var obj = document.getElementById("LIST");
+        var obj2 = document.getElementById("NLIST");
+        if (obj2 != null) {
+            var par = obj2.parentNode;
+            var fc = par.firstChild;
+        }
+        while (fc) {
+            par.removeChild(fc);
+            fc = par.firstChild;
+        }
 
-    const resultsContainer = document.querySelector('#results-container');
-    resultsContainer.classList.remove('hidden');
-  }
+        console.log(json);
+        for (let i of Object.keys(json)) {
 
-  function addKeyValueInput() {
-    const container = document.createElement('div');
-    container.className = 'body-row';
+            let node = document.createElement("div");
+            let node2 = document.createElement("div");
+            let text1 = document.createTextNode(json[i].name + "/" + json[i].email + "/" + json[i].number);
+            let text2 = document.createTextNode(json[i].name);
+            console.log(i);
+            if (json[i].owner == uName.value) {
+                node.setAttribute("id", "LIST");
+                node2.setAttribute("id", "NLIST");
+                node.appendChild(text1);
+                node2.appendChild(text2);
+                resultsDiv.appendChild(node);
+                nameListDiv.appendChild(node2);
+            }
+        }
 
-    const key = document.createElement('input');
-    key.type = 'text';
-    key.className = 'key';
-    key.placeholder = 'key';
-    key.addEventListener('keyup', createRequestPreview);
+        // const resultsContainer = document.querySelector('#results-container');
+    }
 
-    const value = document.createElement('input');
-    value.type = 'text';
-    value.className = 'value';
-    value.placeholder = 'value';
-    value.addEventListener('keyup', createRequestPreview);
 
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => {
-      container.remove();
-      createRequestPreview();
+    function addKeyValueInput() {
+        const container = document.createElement('div');
+        container.className = 'body-row';
+
+        const key = document.createElement('input');
+        key.type = 'text';
+        key.className = 'key';
+        key.placeholder = 'key';
+
+        const value = document.createElement('input');
+        value.type = 'text';
+        value.className = 'value';
+        value.placeholder = 'value';
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.addEventListener('click', () => {
+            container.remove();
+            createRequestPreview();
+        });
+
+        container.append(key);
+        container.append(' : ');
+        container.append(value);
+        container.append(removeButton);
+        const keysContainer = document.querySelector('#key-values');
+        keysContainer.append(container);
+    }
+
+    function getParameters() {
+        const path = pathInput.value.trim();
+
+        const index = methodInput.selectedIndex;
+        const method = methodInput.options[index].value;
+        const options = {
+            method: method
+        };
+
+        const bodyDataContainer = document.querySelector('#key-values');
+        const allRows = bodyDataContainer.querySelectorAll('.body-row');
+        const bodyObj = {};
+        for (let i = 0; i < allRows.length; i++) {
+            const row = allRows[i];
+            const keyInput = row.querySelector('.key').value.trim();
+            const valueInput = row.querySelector('.value').value.trim();
+            if (keyInput && valueInput) {
+                bodyObj[keyInput] = valueInput;
+            }
+        }
+        const bodySize = Object.keys(bodyObj).length;
+        if (bodySize > 0) {
+            options.body = JSON.stringify(bodyObj);
+            options.headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            };
+        }
+        return { path, options };
+    }
+
+
+    const pathInput = document.querySelector('#path-input');
+    const methodInput = document.querySelector('#method-input');
+
+    const addButton = document.querySelector('#add-button');
+    addButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        addKeyValueInput();
     });
 
-    container.append(key);
-    container.append(' : ');
-    container.append(value);
-    container.append(removeButton);
-    const keysContainer = document.querySelector('#key-values');
-    keysContainer.append(container);
-  }
-
-  function getParameters() {
-    const path = pathInput.value.trim();
-
-    const index = methodInput.selectedIndex;
-    const method = methodInput.options[index].value;
-    const options = {
-      method: method
-    };
-
-    const bodyDataContainer = document.querySelector('#key-values');
-    const allRows = bodyDataContainer.querySelectorAll('.body-row');
-    const bodyObj = {};
-    for (let i = 0; i < allRows.length; i++) {
-      const row = allRows[i];
-      const keyInput = row.querySelector('.key').value.trim();
-      const valueInput = row.querySelector('.value').value.trim();
-      if (keyInput && valueInput) {
-        bodyObj[keyInput] = valueInput;
-      }
-    }
-    const bodySize = Object.keys(bodyObj).length;
-    if (bodySize > 0) {
-      options.body = JSON.stringify(bodyObj);
-      options.headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      };
-    }
-    return { path, options };
-  }
-
-  function createRequestPreview() {
-    const info = getParameters();
-    const optionsPretty = JSON.stringify(info.options, null, 2);
-    const previewArea = document.querySelector('#preview-area');
-    previewArea.innerHTML = `fetch('${info.path}', ${optionsPretty});`
-  }
-
-  const pathInput = document.querySelector('#path-input');
-  pathInput.addEventListener('keyup', createRequestPreview);
-  const methodInput = document.querySelector('#method-input');
-  methodInput.addEventListener('change', createRequestPreview);
-
-  const addButton = document.querySelector('#add-button');
-  addButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    addKeyValueInput();
-  });
-
-  const form = document.querySelector('form');
-  form.addEventListener('submit', onSubmit);
-  createRequestPreview();
+    const form = document.querySelector('form');
+    form.addEventListener('submit', onSubmit);
+    const form2 = document.querySelectorAll('form')[1];
+    form2.addEventListener('submit', onSubmit);
 
 })();
+
+function getText() {
+
+    var text = document.getElementById('fake').value; 
+    let uName = document.querySelector('#userName');
+    document.getElementById('path-input').value ="/api/"+uName.value+"/"+text;
+
+}
--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const bodyParser = require('body-parser');
 const googleSheets = require('gsa-sheets');
 
 const key = require('./privateSettings.json');
-
-// TODO(you): Change the value of this string to the spreadsheet id for your
-// GSA spreadsheet. See HW5 spec for more information.
 const SPREADSHEET_ID = '15zl8QaCvHDMXizKLTSNZwV9OK4ZAEP6H99let8F7NIc';
 
 const app = express();
@@ -15,6 +12,7 @@ const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
 app.use(express.static('public'));
 
 async function onGet(req, res) {
+  console.log("method:get");
   const result = await sheet.getRows();
   const rows = result.rows;
   console.log(rows);
@@ -27,22 +25,51 @@ async function onGet(req, res) {
       json[rows[0][j]] = rows[i][j];
     }
     array.push(json);;
-    console.log(array);
-    console.log(json);
   }
   res.json(array);
 }
 app.get('/api', onGet);
 
+async function onDelete(req, res) {
+  console.log("method:detele");
+  const column = req.params.column;
+  const value = req.params.value;
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  var del=0;
+  console.log(column);
+  for (let i = 0; i < rows.length; i++) {
+      if (rows[i][0] == column && rows[i][1]==value) {
+        await sheet.deleteRow(i);
+        del=1;
+        break;
+      }   
+    if(del==1)
+     break;
+  }
+  const result2 = await sheet.getRows();
+  const rows2 = result2.rows;
+  var array = [];
+
+  for (var i = 1; i < rows2.length; i++) {
+    let json = {};
+    for (var j = 0; j < rows2[0].length; j++) {
+      json[rows2[0][j]] = rows2[i][j];
+    }
+    array.push(json);;
+  }
+  res.json(array);
+}
+app.delete('/api/:column/:value', onDelete);
+
 async function onPost(req, res) {
+  console.log("method:post");
   const messageBody = req.body;
   const idList = Object.keys(messageBody);
   var nameList = Object.values(messageBody);
   var newrow = [];
   const result = await sheet.getRows();
   const rows = result.rows;
-  console.log(idList);
-  console.log(nameList);
   if (idList.length == rows[0].length) {
     for (var i = 0; i < rows.length; i++) {
       for (var j = 0; j < rows[0].length; j++) {
@@ -51,80 +78,28 @@ async function onPost(req, res) {
         }
       }
     }
-    res.json({ "response": "success" });
   }
   else{
-    res.json({ "response": "success" });
   }
-  sheet.appendRow(newrow);
+  await sheet.appendRow(newrow);
   console.log(newrow);
+  const result2 = await sheet.getRows();
+  const rows2 = result2.rows;
+  var array = [];
+
+  for (var i = 1; i < rows2.length; i++) {
+    let json = {};
+    for (var j = 0; j < rows2[0].length; j++) {
+      json[rows2[0][j]] = rows2[i][j];
+    }
+    array.push(json);;
+  }
+  res.json(array);
 
 }
 app.post('/api', jsonParser, onPost);
 
-async function onPatch(req, res) {
-  const column = req.params.column;
-  const value = req.params.value;
-  const messageBody = req.body;
-  const idList = Object.keys(messageBody);
-  var nameList = Object.values(messageBody);
-  const result = await sheet.getRows();
-  const rows = result.rows;
-  var json = {};
-  var newrow = [];
-  var index=0;
-
-  for (let i = 0; i < rows[0].length; i++)
-  {
-    if(column==rows[0][i])
-    {
-      colindex=i;
-      break;
-    }
-  }
-  for (let i = 0; i < rows.length; i++) {
-      if (rows[i][colindex] == value) {
-        index=i;
-        break;
-      } 
-  }
-  for (let i = 0; i < rows[0].length; i++) {
-    {
-      newrow[i]=rows[index][i];
-      for (let j=0 ; j< idList.length ; j++)
-      {
-          if(idList[j] == rows[0][i])
-          {
-            newrow[i] = nameList[j];
-            json[rows[0][i]]=nameList[j];
-            break;
-          }
-      }
-    }
-  }
-  res.json(json);
-  sheet.setRow(index,newrow);
-  console.log(newrow);
-}
-app.patch('/api/:column/:value', jsonParser, onPatch);
-
-async function onDelete(req, res) {
-  const column = req.params.column;
-  const value = req.params.value;
-  const result = await sheet.getRows();
-  const rows = result.rows;
-  for (let i = 0; i < rows.length; i++) {
-    for (let j = 0; j < rows[i].length; j++) {
-      if (rows[i][j] == value) {
-        sheet.deleteRow(i);
-        break;
-      }
-    }
-  }
-  res.json({ "response": "success" });
-}
-app.delete('/api/:column/:value', onDelete);
-
+// TODO(you): Add at least 1 GET route and 1 POST route.
 
 // Please don't change this; this is needed to deploy on Heroku.
 const port = process.env.PORT || 3000;
@@ -132,3 +107,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log(`Server listening on port ${port}!`);
 });
+
+
